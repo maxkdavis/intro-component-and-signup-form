54442d52a0ac6b0bef299f0c6edf64622ec08e22
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2025-02-12 07:57:11 -0500

    form functionality updates

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const formEl = document.getElementById('form');
 
 formEl.addEventListener('submit', function (e) {
   e.preventDefault();
-  const firstName = document.getElementById('firstName');
+  const firstNameInputEl = document.getElementById('firstName');
 
-  if (!firstName.value) {
-    firstName.classList.add('error');
+  if (!firstNameInputEl.value) {
+    setError(firstNameInputEl, 'First name cannot be blank');
   }
 });
+
+function setError(input, message) {
+  const inputParent = input.parentElement;
+  const smallEl = inputParent.querySelector('small');
+
+  input.placeholder = '';
+
+  smallEl.style.visibility = 'visible';
+  smallEl.innerText = message;
+
+  inputParent.className = 'form-control error';
+  // checks
+  //it did work for input and small
+}
